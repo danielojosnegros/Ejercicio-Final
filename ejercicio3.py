4c1eca3bdce1d8a004d5221ae3e0fc1356f26d48
Daniel <danielojosnegros@telefonica.net>
2025-11-25 08:42:14 +0100

    cambio ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,8 +1,15 @@
 #importo la clase random para generar numeros aleatorios.
 import random
-input("Presione intro para lanzar: ")
-j1dados1 = random.randint(1,6)
-j1datos2 = random.randint(1,6)
-print(j1dados1, j1datos2)
+input("Presione intro para lanzar el jugador 1: ")
+dado1j1 = random.randint(1,6)
+dado2j1 = random.randint(1,6)
+input("Presione intro para lanzar el jugador 2: ")
+dado1j2 = random.randint(1,6)
+dado2j2 = random.randint(1,6)
+print(dado1j1)
+print(dado2j1)
+print(dado1j2)
+print(dado2j2)
+
 
 
